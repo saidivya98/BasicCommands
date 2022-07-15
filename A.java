452ef1640b42d477626882727018a70b3cda91d5
saidivya98 <108919317+saidivya98@users.noreply.github.com>
2022-07-15 23:14:03 +0530

    Update A.java

--- A.java
+++ A.java
@@ -1,8 +1,8 @@
 public class A{
 	public static void main(String[] h){
-		System.ou.println("Welcome to branch-1");
+		System.out.println("Welcome to branch-1");
 		public static hii(){
-		System.ou.println("Welcome to branch-1");
+		System.out.println("Welcome to branch-1");
 	}
 
 }
